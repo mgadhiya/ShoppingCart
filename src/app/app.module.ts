4528044f11fbc2b03b37395c42e575f0ce06c6bb
miragadhiya <mira.gadhiya@ryzeclaims.com>
2018-02-05 15:39:03 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+import { AdminModule } from './admin/admin.module';
+import { AdminRoutingModule } from './admin/admin-routing.module';
+
 
 
 @NgModule({
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+   // AdminModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
